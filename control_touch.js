46db1e1008b405de841b388fab512a28a74bb839
Anton Plastun <p.anton070793@gmail.com>
2022-11-12 22:10:15 +0300

    fix: bagg deth

--- control_touch.js
+++ control_touch.js
@@ -8,63 +8,137 @@ let right_player2 = document.querySelector('.right_player2')
 let left_player2 = document.querySelector('.left_player2')
 let up_player2 = document.querySelector('.up_player2')
 
+if(gameStatus === 1){
+    attack_btn_player1.addEventListener("touchstart",function(){
+        Player1.attacking();
+        console.log('attack')    
+    },false)
+    attack_btn_player2.addEventListener("touchstart",function(){
+        Player2.attacking();
+        console.log('attack')    
+    },false)
 
-attack_btn_player1.addEventListener("touchstart",function(){
-    Player1.attacking();
-    console.log('attack')    
-},false)
-attack_btn_player2.addEventListener("touchstart",function(){
-    Player2.attacking();
-    console.log('attack')    
-},false)
+    attack_btn_player1.addEventListener("touchend",function(){
+        Player1.attack = false 
+    },false)
+    attack_btn_player2.addEventListener("touchend",function(){
+        Player2.attack = false
+            
+    },false)
 
-attack_btn_player1.addEventListener("touchend",function(){
-    Player1.attack = false 
-},false)
-attack_btn_player2.addEventListener("touchend",function(){
-    Player2.attack = false
+
+    right_player1.addEventListener("touchstart",function(){
+        Player1.speed.x = 5;   
+    },false);
+    right_player1.addEventListener("touchend",function(){
+        Player1.speed.x = 0;   
+    },false)
+
+    left_player1.addEventListener("touchstart",function(){
+        Player1.speed.x = -5;   
+    },false);
+    left_player1.addEventListener("touchend",function(){
+        Player1.speed.x = 0;   
+    },false)
+    up_player1.addEventListener("touchstart",function(){
+        Player1.speed.y = -10;   
+    },false);
+
+
+
+
+    right_player2.addEventListener("touchstart",function(){
+        Player2.speed.x = 5;   
+    },false);
+    right_player2.addEventListener("touchend",function(){
+        Player2.speed.x = 0;   
+    },false)
+
+    left_player2.addEventListener("touchstart",function(){
+        Player2.speed.x = -5;   
+    },false);
+    left_player2.addEventListener("touchend",function(){
+        Player2.speed.x = 0;   
+    },false)
+
+    up_player2.addEventListener("touchstart",function(){
+        Player2.speed.y = -10;   
+    },false);
+
+}
+
+// обработчики ударов и передвижения
+document.addEventListener('keydown',(EO)=>{
+    EO=EO||window.event;
+    console.log(EO.key)
+    if(gameStatus === 1)
+        switch(EO.key) {
+            case 'd':
+    
+                Player1.speed.x = 5;
+            break
+            case 'a':
         
-},false)
+                Player1.speed.x = -5;
+            break 
+            case 'w':
 
+                Player1.speed.y = -10;
+            break 
+            case 'ArrowRight':
 
-right_player1.addEventListener("touchstart",function(){
-    Player1.speed.x = 5;   
-},false);
-right_player1.addEventListener("touchend",function(){
-    Player1.speed.x = 0;   
-},false)
+                Player2.speed.x = 5;
+            break
+            case 'ArrowLeft':
 
-left_player1.addEventListener("touchstart",function(){
-    Player1.speed.x = -5;   
-},false);
-left_player1.addEventListener("touchend",function(){
-    Player1.speed.x = 0;   
-},false)
-up_player1.addEventListener("touchstart",function(){
-    Player1.speed.y = -10;   
-},false);
+                Player2.speed.x = -5;
+            break
+            case 'ArrowUp':
 
+                Player2.speed.y = -10;
+            break
+            case 's':
 
+                Player1.attacking();
+            break
+            case 'ArrowDown':
 
+                Player2.attacking();
+            break
 
-right_player2.addEventListener("touchstart",function(){
-    Player1.speed.x = 5;   
-},false);
-right_player2.addEventListener("touchend",function(){
-    Player2.speed.x = 0;   
-},false)
+        }
+   
+})
 
-left_player2.addEventListener("touchstart",function(){
-    Player2.speed.x = -5;   
-},false);
-left_player2.addEventListener("touchend",function(){
-    Player2.speed.x = 0;   
-},false)
+document.addEventListener('keyup',(EO)=>{
+    EO=EO||window.event;
+    switch(EO.key) {
+        case 'd':
+            Player1.speed.x = 0;
+        break
+        case 'a':
+            Player1.speed.x = 0;
+        break
+        case 'ArrowRight':
 
-up_player2.addEventListener("touchstart",function(){
-    Player2.speed.y = -10;   
-},false);
+            Player2.speed.x = 0;
+        break
+        case 'ArrowLeft':
 
+            Player2.speed.x = 0;
+        break
+        case 's':
+
+            Player1.attack = false;
+        break
+        case 'ArrowDown':
+  
+            Player2.attack = false;
+        break     
+    }    
+    }
+   
+)
 
 
 
--- script.js
+++ script.js
@@ -379,78 +379,7 @@ function  choiceSprite(sprite,player){
     }
 }
 
-// обработчики ударов и передвижения
-document.addEventListener('keydown',(EO)=>{
-    EO=EO||window.event;
-    console.log(EO.key)
-    if(gameStatus === 1)
-        switch(EO.key) {
-            case 'd':
-    
-                Player1.speed.x = 5;
-            break
-            case 'a':
-        
-                Player1.speed.x = -5;
-            break 
-            case 'w':
 
-                Player1.speed.y = -10;
-            break 
-            case 'ArrowRight':
-
-                Player2.speed.x = 5;
-            break
-            case 'ArrowLeft':
-
-                Player2.speed.x = -5;
-            break
-            case 'ArrowUp':
-
-                Player2.speed.y = -10;
-            break
-            case 's':
-
-                Player1.attacking();
-            break
-            case 'ArrowDown':
-
-                Player2.attacking();
-            break
-
-        }
-   
-})
-
-document.addEventListener('keyup',(EO)=>{
-    EO=EO||window.event;
-    switch(EO.key) {
-        case 'd':
-            Player1.speed.x = 0;
-        break
-        case 'a':
-            Player1.speed.x = 0;
-        break
-        case 'ArrowRight':
-
-            Player2.speed.x = 0;
-        break
-        case 'ArrowLeft':
-
-            Player2.speed.x = 0;
-        break
-        case 's':
-
-            Player1.attack = false;
-        break
-        case 'ArrowDown':
-  
-            Player2.attack = false;
-        break     
-    }    
-    }
-   
-)
 
 
 
