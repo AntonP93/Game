1f9330db541c1b0241453e7ef7cf9300ec481230
Anton Plastun <p.anton070793@gmail.com>
2022-11-12 22:01:16 +0300

    file: all toch move

--- control_touch.js
+++ control_touch.js
@@ -1,5 +1,13 @@
 let attack_btn_player1 = document.querySelector('.attack_btn_player1')
 let attack_btn_player2 = document.querySelector('.attack_btn_player2')
+let right_player1 = document.querySelector('.right')
+let left_player1 = document.querySelector('.left')
+let up_player1 = document.querySelector('.up')
+
+let right_player2 = document.querySelector('.right_player2')
+let left_player2 = document.querySelector('.left_player2')
+let up_player2 = document.querySelector('.up_player2')
+
 
 attack_btn_player1.addEventListener("touchstart",function(){
     Player1.attacking();
@@ -9,6 +17,7 @@ attack_btn_player2.addEventListener("touchstart",function(){
     Player2.attacking();
     console.log('attack')    
 },false)
+
 attack_btn_player1.addEventListener("touchend",function(){
     Player1.attack = false 
 },false)
@@ -16,6 +25,45 @@ attack_btn_player2.addEventListener("touchend",function(){
     Player2.attack = false
         
 },false)
+
+
+right_player1.addEventListener("touchstart",function(){
+    Player1.speed.x = 5;   
+},false);
+right_player1.addEventListener("touchend",function(){
+    Player1.speed.x = 0;   
+},false)
+
+left_player1.addEventListener("touchstart",function(){
+    Player1.speed.x = -5;   
+},false);
+left_player1.addEventListener("touchend",function(){
+    Player1.speed.x = 0;   
+},false)
+up_player1.addEventListener("touchstart",function(){
+    Player1.speed.y = -10;   
+},false);
+
+
+
+
+right_player2.addEventListener("touchstart",function(){
+    Player1.speed.x = 5;   
+},false);
+right_player2.addEventListener("touchend",function(){
+    Player2.speed.x = 0;   
+},false)
+
+left_player2.addEventListener("touchstart",function(){
+    Player2.speed.x = -5;   
+},false);
+left_player2.addEventListener("touchend",function(){
+    Player2.speed.x = 0;   
+},false)
+
+up_player2.addEventListener("touchstart",function(){
+    Player2.speed.y = -10;   
+},false);
 
 
 
